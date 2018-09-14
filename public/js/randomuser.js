73eb18f2aa48c648fb8eebf3d66e4fede9c2e8ac
Allyson Carroll <allcarro@iu.edu>
2018-09-13 22:09:38 -0400

    homework three complete

--- public/js/randomuser.js
+++ public/js/randomuser.js
@@ -1,86 +1,60 @@
-var randomuser = new VUE({
-  el: 'randomuser',
-  "results": [
+var randomuser = new Vue({
+  el: "#randomuser",
+  data:{
+  "person":
     {
-      "gender": "male",
       "name": {
-        "title": "mr",
-        "first": "rolf",
-        "last": "hegdal"
+        "title": "",
+        "first": "",
+        "last": ""
       },
       "location": {
-        "street": "ljan terrasse 346",
-        "city": "vear",
-        "state": "rogaland",
-        "postcode": "3095",
-        "coordinates": {
-          "latitude": "54.8646",
-          "longitude": "-97.3136"
-        },
-        "timezone": {
-          "offset": "-10:00",
-          "description": "Hawaii"
-        }
-      },
-      "email": "rolf.hegdal@example.com",
-      "login": {
-        "uuid": "c4168eac-84b8-46ea-b735-c9da9bfb97fd",
-        "username": "bluefrog786",
-        "password": "ingrid",
-        "salt": "GtRFz4NE",
-        "md5": "5c581c5748fc8c35bd7f16eac9efbb55",
-        "sha1": "c3feb8887abed9ec1561b9aa2c9f58de21d1d3d9",
-        "sha256": "684c478a98b43f1ef1703b35b8bbf61b27dbc93d52acd515e141e97e04447712"
-      },
-      "dob": {
-        "date": "1975-11-12T06:34:44Z",
-        "age": 42
-      },
-      "registered": {
-        "date": "2015-11-04T22:09:36Z",
-        "age": 2
-      },
-      "phone": "66976498",
-      "cell": "40652479",
-      "id": {
-        "name": "FN",
-        "value": "12117533881"
-      },
-      "picture": {
-        "large": "https://randomuser.me/api/portraits/men/65.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/65.jpg",
-        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/65.jpg"
-      },
-      "nat": "NO"
-    }
-  ],
-  "info": {
-    "seed": "2da87e9305069f1d",
-    "results": 1,
-    "page": 1,
-    "version": "1.2"
+        "street": "",
+        "city": "",
+        "state": "",
+        "postcode": ""},
 
+      "email": "",
+
+      "dob": {
+        "date": "",
+        "age": ""
+      },
+
+      "picture": {
+        "large": "",
+        "medium": "",
+        "thumbnail": ""
+      }
+    }
+  },
+
+computed:{
+  todateage: function(){
+    return moment().diff(moment(this.person.dob.date), 'years');
   }
-})
+},
 
 methods: {
-  fetchuser () {
+  pretty_date: function(d)
+  {
+    return moment(d).format('1');
+  },
+  fetchuser : function() {
   fetch('https://randomuser.me/api/')
   .then( response => response.json() )
-  .then( json => {randomuser.project = json} )
-  .catch( err => {
+  .then( json => {
+    randomuser.person = json.results[0];
+  console.log(randomuser.person);
+})
+
+  .catch(function (err) {
     console.log('PROJECT FETCH ERROR:');
     console.log(err);
   })
-}
-}
+}},
+
 created () {
-this.fetchProject();
-this.fetchTasks();
+this.fetchuser();
 }
-var reloadlink = new Vue({
-  el: '#reloadlink',
-  data: {
-    counter:
-  }
 })
